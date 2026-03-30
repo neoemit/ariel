6cc70eebc09a26693b145badf5e05a3065de9187
Thomas La Mendola <thomas.lamendola@gmail.com>
2026-03-30 12:55:50 +0200

    Refresh launcher icon resources and bump app version to 1.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     namespace = "com.thomaslamendola.ariel"
     compileSdk = 35
 
-    val appVersionMinor = 25
+    val appVersionMinor = 26
 
     defaultConfig {
         applicationId = "com.thomaslamendola.ariel"
